2815979823037bafd6deba8698bd31c7b85919d4
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-10-22 14:13:42 -0400

    Loading/saving of fields

---
+++ scripts/Main.gd
@@ -0,0 +1,23 @@
+extends Control
+	
+func save_lang(filename: String, info: Dictionary):
+	var save_file = FileAccess.open(filename, FileAccess.WRITE)
+	save_file.store_line(JSON.new().stringify(info))
+	save_file.close()
+	
+func load_lang(filename: String) -> Dictionary:
+	var load_file = FileAccess.open(filename, FileAccess.READ)
+	var lines = load_file.get_as_text()
+	return JSON.new().parse_string(lines)
+	
+func collate_data() -> Dictionary:
+	var info: Dictionary
+	info["LanguageName"] = (get_node("TabManager/ProjectMenu/NameOfLanguage") as LineEdit).text
+	info["Autonym"] = (get_node("TabManager/ProjectMenu/Autonym") as LineEdit).text
+	info["LanguageType"] = (get_node("TabManager/ProjectMenu/Langtype") as OptionButton).selected
+	return info
+	
+func load_data(info: Dictionary):
+	(get_node("TabManager/ProjectMenu/NameOfLanguage") as LineEdit).text = info["LanguageName"]
+	(get_node("TabManager/ProjectMenu/Autonym") as LineEdit).text = info["Autonym"]
+	(get_node("TabManager/ProjectMenu/Langtype") as OptionButton).selected = info["LanguageType"]
