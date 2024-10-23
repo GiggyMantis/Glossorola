9e32fe59959c7624e15be73af87dabdf6a05bd9d
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-10-23 19:18:49 -0400

    working SCA-like rewrite rules

--- scripts/SCA.gd
+++ scripts/SCA.gd
@@ -1,6 +1,6 @@
 class_name SCA
 
-func apply(lexicon: String, rules: String, categories: String, rewrite_rules: String, apply_rewrite_rules_on_output: bool) -> String:
+static func apply(lexicon: String, rules: String, categories: String, rewrite_rules: String, apply_rewrite_rules_on_output: bool) -> String:
 	var ret = lexicon
 	ret = rewrite(ret, rewrite_rules, false)
 	
@@ -8,6 +8,25 @@ func apply(lexicon: String, rules: String, categories: String, rewrite_rules: St
 		ret = rewrite(ret, rewrite_rules, true)
 	return ret
 	
-func rewrite(s: String, rules: String, reverse: bool) -> String:
+static func rewrite(s: String, rules: String, reverse: bool) -> String:
+	
+	var rules_array = rules.split("\n")
+	var replacement_left: Array[String]
+	var replacement_right: Dictionary
+	for rule in rules_array:
+		var rule_individual = rule.split("|")
+		if not reverse:
+			replacement_left.append(rule_individual[0])
+			replacement_right[rule_individual[0]] = (rule_individual[1])
+		else:
+			replacement_left.append(rule_individual[1])
+			replacement_right[rule_individual[1]] = (rule_individual[0])
+	
+	if reverse:
+		replacement_left.reverse()
+	
 	var ret = s
+	for rule in replacement_left:
+		ret = ret.replace(rule, replacement_right[rule])
+	
 	return ret
