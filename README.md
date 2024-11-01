086479f23d51eded12b887197e2863f51874fda1
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-11-01 15:28:31 -0400

    Chronology backend

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 Glossorola is a desktop app for the creation of [constructed languages](https://en.wikipedia.org/wiki/Constructed_language).
 It consists of several modules, such as the Dictionary Module, Phonology Module, Grammar Module, and Evolution Module. 
 It allows for high-efficiency diachronic and synchronic conlanging, as well as providing tools for creating high-quality documentation and visuals.
+
+# Warnings
+
+The evolution module is sensitive to years only. If you have two sound changes that need to be applied in a particular order, and you put them in the same year, it may cause unintended effects.
+Consider changing them to one, larger sound change mutation or seperating them by 1 year.
--- scripts/Chronology.gd
+++ scripts/Chronology.gd
@@ -3,9 +3,9 @@ class_name Chronology
 
 const MAX_YEAR: int = 9000000000000000000
 
-var list: Array[Mutation]
+var list: Array
 
-static func New(list: Array[Mutation]) -> Chronology:
+static func New(list: Array) -> Chronology:
 	var ret = Chronology.new()
 	ret.list = list
 	return ret
@@ -14,12 +14,23 @@ func sort() -> void:
 	list.sort_custom(self.compare_events)
 
 # Returns true if the first element should be moved before the second one, otherwise returns false.
-func compare_events(a: Mutation, b: Mutation) -> bool:
+func compare_events(a, b) -> bool:
 	return true if a.year < b.year else false
 
 func get_final_year() -> int:
 	self.sort()
 	return list[-1].year
+	
+# Evolves up to and including year
+func evolve(dict: Dictionary, year: int) -> Dictionary:
+	var ret = dict
+	assert(year <= MAX_YEAR)
+	sort()
+	for mutation in list:
+		if mutation.year > year:
+			break
+		ret = mutation._mutate()
+	return ret
 
 func _to_string() -> String:
 	self.sort()
